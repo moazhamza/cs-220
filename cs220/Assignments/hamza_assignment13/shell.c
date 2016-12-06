b2ea9653035e461f31ffe6e74d3d57aab687b7c3
mhamza <moazhamza@gmail.com>
2016-12-06 15:45:09 -0500

    Assignment 13 part 2 completed

--- cs220/Assignments/hamza_assignment13/shell.c
+++ cs220/Assignments/hamza_assignment13/shell.c
@@ -5,70 +5,78 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-char **split_on_spaces(char *str){
-	    int i, numArgs = 1, j=0;
-	    for(i=0; i < strlen(str); i++) if(str[i] == ' ') numArgs++;
-	    char **args = malloc(numArgs * sizeof(char));
-	    i = 0;
-	    while(str[i] != '\n' && str[i] != '\0'){
-		    if(str[i] == ' ') {
-			    str[i] = '\0';
-			    args[j] = str;
-			    str = &(str[i+1]);
-			    j++; i=0;
-		    }
-		    else{    
-			    i++;
-		    }
-	    }
-	    args[j] = str; 
-	    args[j+1] = NULL;
-	    return args;
+char **set_up_args(char *str, char **args){
+    int i, numArgs = 1, j=0;
+    i = 0;
+    while(str[i] != '\n' && str[i] != '\0'){
+        if(str[i] == ' ') {
+            str[i] = '\0';
+            args[j] = str;
+            str = &(str[i+1]);
+            j++; i=0;
+        }
+        else{
+            i++;
+        }
+    }
+    args[j] = str;
+    args[j+1] = NULL;
+    return args;
+}
+/* Returns the second argument if it exists,
+ * returns 0 if there is no second argument.
+ */
+char *set_up_second_command(char *line, char *secondCommand){
+    int i;
+    for (i=0; i< strlen(line); i++) {
+        if(line[i] == '&' && line[i+1] == '&'){
+            line[i-1] = '\0';
+            secondCommand = &line[i+3];
+            return secondCommand;
+        }
+    }
+    return 0;
 }
 
 int main(){
-	char line[1024];
-	int pid, i, error=0;
-	unsigned char multipleCommands = 0;
-	char *secondLine;
-	while(1){
-		printf("Hello!!>");
-		if(!fgets(line, 1023, stdin)){
-			break;
-		}
-		if(strcmp(line, "exit\n")  == 0) break;
-		for (i=0; i< strlen(line); i++) {
-			if(line[i] == '\n') line[i] = '\0';
-		}
-		for (i=0; i< strlen(line); i++) {
-			if(line[i] == '&' && line[i+1] == '&'){
-				line[i] == '\0';
-				secondLine = &(line[i+3]);
-				multipleCommands = 1;
-			}	
-		}
-		char **args = split_on_spaces(line);
-		
-		pid = fork();
-		if(pid == 0) { /* This is the child */
-			error = execvp(line, args);
-			if(multipleCommands && error != -1){
-				char **args2 = split_on_spaces(line);
-				pid = fork();
-				if(pid == 0){
-					execvp(secondLine, args2);
-					fprintf(stderr, "Hello!!: %s\n", strerror(errno));
-					exit(errno);
-			}else{
-					wait(NULL);
+    char line[1024];
+    int pid, pid2, i, error;
+    char *secondCommand = 0; 
+    char **args = (char **) malloc(1024);
+    char **args2 = (char **) malloc(1024);
+    while(1){
+        printf("Hello!!>");
+        if(!fgets(line, 1023, stdin)){
+            break;
+        }
+        if(strcmp(line, "exit\n")  == 0) break;
+        for (i=0; i< strlen(line); i++) {
+            if(line[i] == '\n') line[i] = '\0';
+        }
+	
+       	secondCommand = set_up_second_command(line, secondCommand);
+        args = set_up_args(line, args);
+        
+        pid = fork();
+        if(pid == 0) { /* This is the child */ 	
+		execvp(line, args);
+		fprintf(stderr, "Hello!!: %s\n", strerror(errno));
+		exit(errno);
+	} else {
+            wait(&error);
+        }
+	if(secondCommand && !error){
+		pid2 = fork();
+		if(pid2 == 0){ /*This is another child */
+			if(errno == 0){		 	
+				args2 = set_up_args(secondCommand, args2);
+				execvp(secondCommand, args2);
+				fprintf(stderr, "Hello!!: %s\n", strerror(errno));
+				exit(errno);
 			}
-			fprintf(stderr, "Hello!!: %s\n", strerror(errno));
-			exit(errno);
-			}
-		} else {
-			wait(NULL);
-		}
+		}else wait(NULL);
 	}
-	return 0;
+    }
+    
+    return 0;
 }
-
