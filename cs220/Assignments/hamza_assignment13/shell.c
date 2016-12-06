a82d858e06d36e8d279f78d1d393d6b18adc239d
mhamza <moazhamza@gmail.com>
2016-12-05 22:53:12 -0500

    Assignment 13 part 1 completed, attempted part 2

--- cs220/Assignments/hamza_assignment13/shell.c
+++ cs220/Assignments/hamza_assignment13/shell.c
@@ -5,33 +5,66 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+char **split_on_spaces(char *str){
+	    int i, numArgs = 1, j=0;
+	    for(i=0; i < strlen(str); i++) if(str[i] == ' ') numArgs++;
+	    char **args = malloc(numArgs * sizeof(char));
+	    i = 0;
+	    while(str[i] != '\n' && str[i] != '\0'){
+		    if(str[i] == ' ') {
+			    str[i] = '\0';
+			    args[j] = str;
+			    str = &(str[i+1]);
+			    j++; i=0;
+		    }
+		    else{    
+			    i++;
+		    }
+	    }
+	    args[j] = str; 
+	    args[j+1] = NULL;
+	    return args;
+}
+
 int main(){
 	char line[1024];
-	int pid, i;
-        char *args[100];
+	int pid, i, error=0;
+	unsigned char multipleCommands = 0;
+	char *secondLine;
 	while(1){
 		printf("Hello!!>");
 		if(!fgets(line, 1023, stdin)){
 			break;
 		}
 		if(strcmp(line, "exit\n")  == 0) break;
-
-		unsigned int numArgs = 0;
-		args[0] = &line;
-		for (i=0; i < strlen(line); i++) if (line[i] == '\n') line[i] = '\0';	
-		for (i=0; i < strlen(line); i++){
-			if (line[i] == ' ') {
-				line[i] = '\0';
-				numArgs++;
-				args[numArgs] = &(line[i+1]);
-			}
-		}			
-		args[numArgs+1] = NULL;
+		for (i=0; i< strlen(line); i++) {
+			if(line[i] == '\n') line[i] = '\0';
+		}
+		for (i=0; i< strlen(line); i++) {
+			if(line[i] == '&' && line[i+1] == '&'){
+				line[i] == '\0';
+				secondLine = &(line[i+3]);
+				multipleCommands = 1;
+			}	
+		}
+		char **args = split_on_spaces(line);
+		
 		pid = fork();
 		if(pid == 0) { /* This is the child */
-			execvp(line, args);
+			error = execvp(line, args);
+			if(multipleCommands && error != -1){
+				char **args2 = split_on_spaces(line);
+				pid = fork();
+				if(pid == 0){
+					execvp(secondLine, args2);
+					fprintf(stderr, "Hello!!: %s\n", strerror(errno));
+					exit(errno);
+			}else{
+					wait(NULL);
+			}
 			fprintf(stderr, "Hello!!: %s\n", strerror(errno));
 			exit(errno);
+			}
 		} else {
 			wait(NULL);
 		}
